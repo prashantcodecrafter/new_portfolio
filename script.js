54fe495a1822598c557823b315d2e13b57efa3ca
prashant kalpure <162542783+prashantcodecrafter@users.noreply.github.com>
2025-11-08 12:55:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+    navLinks.classList.remove('active');
+  });
+});
